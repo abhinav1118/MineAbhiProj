7c5fbd8d1fd6c713e4f3aa6af55a3f64b6f082cd
ABHINAV GOUD <ABHINAV GOUD@ABHI>
2018-07-19 13:21:04 +0530

    ojo

---
+++ src/main/java/com/pojo/Passport.java
@@ -0,0 +1,34 @@
+package com.pojo;
+
+
+public class Passport {
+
+	
+		
+
+		private String name;
+		private String Passnumber;
+		private String git;
+		
+		
+		
+		public String getGit() {
+			return git;
+		}
+		public void setGit(String git) {
+			this.git = git;
+		}
+		public String getName() {
+			return name;
+		}
+		public void setName(String name) {
+			this.name = name;
+		}
+		public String getPassnumber() {
+			return Passnumber;
+		}
+		public void setPassnumber(String passnumber) {
+			Passnumber = passnumber;
+		}
+		
+}
